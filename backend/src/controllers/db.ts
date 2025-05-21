56184bb47b21dcbdc68c498324add87858180101
UtkarshSingh8789 <utkarshkumarsingh42@gmail.com>
2025-05-21 18:05:13 +0530

    database connection

---
+++ backend/src/controllers/db.ts
@@ -0,0 +1,9 @@
+import mongoose from "mongoose"
+const dbConnect = ()=>{
+  mongoose.connect(`${process.env.DBurl}Brainly`).then(()=>{
+    console.log("Connected Successfully")
+  }).catch((err)=>{
+    console.log("Something Wrong",err)
+  })
+}
+export default dbConnect
