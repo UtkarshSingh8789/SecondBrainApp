1e78e751f8f06d4daed5a367bc84c500dcc0cefd
UtkarshSingh8789 <utkarshkumarsingh42@gmail.com>
2025-05-22 18:49:29 +0530

    created Index file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}))
 dotenv.config();
 app.use(cors({
-  origin: '*', 
+  origin: '*',
   credentials: true 
 }));
 app.use(cookieParser());
