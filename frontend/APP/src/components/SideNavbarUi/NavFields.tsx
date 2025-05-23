169c0ef50b3c9a94d306c84840dbec6de9ca8c9a
UtkarshSingh8789 <utkarshkumarsingh42@gmail.com>
2025-05-23 17:12:15 +0530

    NavFields UI

--- frontend/APP/src/components/SideNavbarUi/NavFields.tsx
+++ frontend/APP/src/components/SideNavbarUi/NavFields.tsx
@@ -2,7 +2,6 @@ interface NavFieldProps{
   text: String,
   startIcon: React.ReactElement
 }
-
 const NavFields = (props: NavFieldProps)=>{
   return <div className="flex gap-3 items-center hover:border hover:bg-gray-100 h-[6vh] pl-7">
       <div className="pt-2">
@@ -13,5 +12,4 @@ const NavFields = (props: NavFieldProps)=>{
       </span>
   </div> 
 }
-
 export default NavFields;
