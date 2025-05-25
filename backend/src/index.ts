ed2712bd10a539c9913fa5ed8ef000ae7b8432d6
UtkarshSingh8789 <utkarshkumarsingh42@gmail.com>
2025-05-25 21:48:03 +0530

    changesd index.ts little bit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,6 @@ import cors from "cors";
 import router from "./routes/pageRoutes";
 import dbConnect from "./config/db";
 import cookieParser from 'cookie-parser';
-
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}))
@@ -15,9 +14,7 @@ app.use(cors({
 }));
 app.use(cookieParser());
 dbConnect();
-
 app.use("/api/v1",router);
-
 app.listen(process.env.PORT,()=>{
   console.log(`searver is renning at port ${process.env.PORT}`)
 })
